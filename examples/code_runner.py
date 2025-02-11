b3cb0b91f4229a3085feddfa418d7646cdc9b447
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:25:31 +0000

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- examples/code_runner.py
+++ examples/code_runner.py
@@ -1,105 +1,77 @@
+import os
+from typing import List
 from llm_sandbox import SandboxSession
+from llm_sandbox.utils import get_libraries_installation_command, get_code_execution_command, get_code_file_extension
+from llm_sandbox.const import SupportedLanguage
 
+def run_code(lang: str, code: str, libraries: List[str] = None):
+    output_dir = f'output_{lang}'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-def run_python_code():
-    with SandboxSession(lang="python", keep_template=True, verbose=True) as session:
-        output = session.run("print('Hello, World!')")
-        print(output)
+    with SandboxSession(lang=lang, keep_template=True, verbose=True) as session:
+        if libraries:
+            install_command = get_libraries_installation_command(lang, libraries)
+            session.execute_command(install_command)
 
-        output = session.run(
-            "import numpy as np\nprint(np.random.rand())", libraries=["numpy"]
-        )
-        print(output)
+        code_file = f'code.{get_code_file_extension(lang)}'
+        with open(os.path.join(output_dir, code_file), 'w') as f:
+            f.write(code)
 
-        session.execute_command("pip install pandas")
-        output = session.run("import pandas as pd\nprint(pd.__version__)")
-        print(output)
+        session.copy_to_runtime(code_file, f'/sandbox/{code_file}')
+        execution_commands = get_code_execution_command(lang, code_file)
+        for command in execution_commands:
+            output = session.run(command)
+            print(output)
 
-        session.copy_to_runtime("README.md", "/sandbox/data.csv")
+if __name__ == '__main__':
+    python_code = """
+    print('Hello, World!')
+    import numpy as np
+    print(np.random.rand())
+    import pandas as pd
+    print(pd.__version__)
+    """
+    run_code(SupportedLanguage.PYTHON, python_code, libraries=['numpy', 'pandas'])
 
+    java_code = """
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }
+    """
+    run_code(SupportedLanguage.JAVA, java_code)
 
-def run_java_code():
-    with SandboxSession(lang="java", keep_template=True, verbose=True) as session:
-        output = session.run(
-            """
-            public class Main {
-                public static void main(String[] args) {
-                    System.out.println("Hello, World!");
-                }
-            }
-            """,
-        )
-        print(output)
+    javascript_code = """
+    console.log('Hello, World!')
+    const axios = require('axios');
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(response => console.log(response.data));
+    """
+    run_code(SupportedLanguage.JAVASCRIPT, javascript_code, libraries=['axios'])
 
+    cpp_code = """
+    #include <iostream>
+    int main() {
+        std::cout << "Hello, World!" << std::endl;
+        return 0;
+    }
+    """
+    run_code(SupportedLanguage.CPP, cpp_code)
 
-def run_javascript_code():
-    with SandboxSession(lang="javascript", keep_template=True, verbose=True) as session:
-        output = session.run("console.log('Hello, World!')")
-        print(output)
-
-        output = session.run(
-            """
-            const axios = require('axios');
-            axios.get('https://jsonplaceholder.typicode.com/posts/1')
-                .then(response => console.log(response.data));
-            """,
-            libraries=["axios"],
-        )
-        print(output)
-
-
-def run_cpp_code():
-    with SandboxSession(lang="cpp", keep_template=True, verbose=True) as session:
-        output = session.run(
-            """
-            #include <iostream>
-            int main() {
-                std::cout << "Hello, World!" << std::endl;
-                return 0;
-            }
-            """,
-        )
-        print(output)
-
-        output = session.run(
-            """
-            #include <iostream>
-            #include <vector>
-            int main() {
-                std::vector<int> v = {1, 2, 3, 4, 5};
-                for (int i : v) {
-                    std::cout << i << " ";
-                }
-                std::cout << std::endl;
-                return 0;
-            }
-            """,
-        )
-        print(output)
-
-        # run with libraries
-        output = session.run(
-            """
-            #include <iostream>
-            #include <vector>
-            #include <algorithm>
-            int main() {
-                std::vector<int> v = {1, 2, 3, 4, 5};
-                std::reverse(v.begin(), v.end());
-                for (int i : v) {
-                    std::cout << i << " ";
-                }
-                std::cout << std::endl;
-                return 0;
-            }
-            """,
-            libraries=["libstdc++"],
-        )
-        print(output)
-
-
-if __name__ == "__main__":
-    run_python_code()
-    run_java_code()
-    run_javascript_code()
-    run_cpp_code()
+    cpp_code_with_lib = """
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    int main() {
+        std::vector<int> v = {1, 2, 3, 4, 5};
+        std::reverse(v.begin(), v.end());
+        for (int i : v) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+    """
+    run_code(SupportedLanguage.CPP, cpp_code_with_lib, libraries=['libstdc++'])

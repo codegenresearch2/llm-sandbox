961816220e6bd4a20bfdcf81f9fb1882f73331a6
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:43:48 +0000

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- llm_sandbox/utils.py
+++ llm_sandbox/utils.py
@@ -6,7 +6,16 @@ from docker import DockerClient
 from llm_sandbox.const import SupportedLanguage, DefaultImage, NotSupportedLibraryInstallation, SupportedLanguageValues
 
 def image_exists(client: DockerClient, image: str) -> bool:
-    """Check if a Docker image exists."""
+    """
+    Check if a Docker image exists.
+
+    Parameters:
+    client (DockerClient): Docker client.
+    image (str): Docker image.
+
+    Returns:
+    bool: True if the image exists, False otherwise.
+    """
     try:
         client.images.get(image)
         return True
@@ -16,7 +25,19 @@ def image_exists(client: DockerClient, image: str) -> bool:
         raise e
 
 def get_libraries_installation_command(lang: str, libraries: List[str]) -> Optional[str]:
-    """Get the command to install libraries for the given language."""
+    """
+    Get the command to install libraries for the given language.
+
+    Parameters:
+    lang (str): Programming language.
+    libraries (List[str]): List of libraries.
+
+    Returns:
+    Optional[str]: Installation command.
+
+    Raises:
+    ValueError: If the language is not supported.
+    """
     if lang not in SupportedLanguageValues:
         raise ValueError(f"Language {lang} is not supported")
 
@@ -37,7 +58,18 @@ def get_libraries_installation_command(lang: str, libraries: List[str]) -> Optio
         return f"gem install {' '.join(libraries)}"
 
 def get_code_file_extension(lang: str) -> str:
-    """Get the file extension for the given language."""
+    """
+    Get the file extension for the given language.
+
+    Parameters:
+    lang (str): Programming language.
+
+    Returns:
+    str: File extension.
+
+    Raises:
+    ValueError: If the language is not supported.
+    """
     if lang == SupportedLanguage.PYTHON:
         return "py"
     elif lang == SupportedLanguage.JAVA:
@@ -53,30 +85,64 @@ def get_code_file_extension(lang: str) -> str:
     else:
         raise ValueError(f"Language {lang} is not supported")
 
-def get_code_execution_command(lang: str, code_file: str) -> str:
-    """Get the command to execute the code."""
+def get_code_execution_command(lang: str, code_file: str) -> List[str]:
+    """
+    Get the command to execute the code.
+
+    Parameters:
+    lang (str): Programming language.
+    code_file (str): Path to the code file.
+
+    Returns:
+    List[str]: Execution commands.
+
+    Raises:
+    ValueError: If the language is not supported.
+    """
     if lang == SupportedLanguage.PYTHON:
-        return f"python {code_file}"
+        return [f"python {code_file}"]
     elif lang == SupportedLanguage.JAVA:
-        return f"java -cp . {code_file.split('.')[0]}"
+        return [f"javac {code_file}", f"java {code_file.split('.')[0]}"]
     elif lang == SupportedLanguage.JAVASCRIPT:
-        return f"node {code_file}"
+        return [f"node {code_file}"]
     elif lang == SupportedLanguage.CPP:
-        return f"./{code_file}"
+        return [f"g++ -o {code_file.split('.')[0]} {code_file}", f"./{code_file.split('.')[0]}"]
     elif lang == SupportedLanguage.GO:
-        return f"go run {code_file}"
+        return [f"go run {code_file}"]
     elif lang == SupportedLanguage.RUBY:
-        return f"ruby {code_file}"
+        return [f"ruby {code_file}"]
     else:
         raise ValueError(f"Language {lang} is not supported")
 
 def test_directory_existence(directory: str) -> bool:
-    """Test if a directory exists."""
+    """
+    Test if a directory exists.
+
+    Parameters:
+    directory (str): Directory path.
+
+    Returns:
+    bool: True if the directory exists, False otherwise.
+    """
     import os
     return os.path.isdir(directory)
 
 def run_code(lang: str, code: str, libraries: List[str] = None, test_directory: str = None):
-    """Run the code with the given language and libraries."""
+    """
+    Run the code with the given language and libraries.
+
+    Parameters:
+    lang (str): Programming language.
+    code (str): Code to be executed.
+    libraries (List[str], optional): List of libraries to be installed. Defaults to None.
+    test_directory (str, optional): Directory to be tested for existence. Defaults to None.
+
+    Returns:
+    str: Output of the code execution.
+
+    Raises:
+    ValueError: If the test directory does not exist.
+    """
     if libraries is None:
         libraries = []
 
@@ -98,10 +164,13 @@ def run_code(lang: str, code: str, libraries: List[str] = None, test_directory: 
         install_command = get_libraries_installation_command(lang, libraries)
         container.exec_run(install_command, workdir="/app")
 
-    exec_command = get_code_execution_command(lang, code_file)
-    exit_code, output = container.exec_run(exec_command, workdir="/app")
+    exec_commands = get_code_execution_command(lang, code_file)
+    output = ""
+    for command in exec_commands:
+        exit_code, command_output = container.exec_run(command, workdir="/app")
+        output += command_output.decode()
 
     container.stop()
     container.remove()
 
-    return output.decode()
+    return output

21de07c14f6a94bb9da204888d2ff3a6db0024ed
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 16:56:59 +0000

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- examples/code_runner.py
+++ examples/code_runner.py
@@ -1,72 +1,83 @@
 from llm_sandbox import SandboxSession
 from llm_sandbox.utils import get_libraries_installation_command, get_code_execution_command
 
-def run_code(lang: str, code: str, libraries: list = None):
-    with SandboxSession(lang=lang, keep_template=True, verbose=True) as session:
-        if libraries:
-            install_command = get_libraries_installation_command(lang, libraries)
-            session.execute_command(install_command)
-
-        output = session.run(code)
+def execute_python_code():
+    with SandboxSession(lang='python', keep_template=True, verbose=True) as session:
+        output = session.run("print('Hello, World!')")
         print(output)
 
-def execute_python_code():
-    run_code('python', "print('Hello, World!')")
-    run_code('python', "import numpy as np\nprint(np.random.rand())", libraries=['numpy'])
-    run_code('python', "import pandas as pd\nprint(pd.__version__)", libraries=['pandas'])
-    session.copy_to_runtime('README.md', '/sandbox/data.csv')
+        output = session.run("import numpy as np\nprint(np.random.rand())", libraries=['numpy'])
+        print(output)
+
+        session.execute_command("pip install pandas")
+        output = session.run("import pandas as pd\nprint(pd.__version__)")
+        print(output)
+
+        session.copy_to_runtime('README.md', '/sandbox/data.csv')
 
 def execute_java_code():
-    run_code('java', """
-        public class Main {
-            public static void main(String[] args) {
-                System.out.println("Hello, World!");
+    with SandboxSession(lang='java', keep_template=True, verbose=True) as session:
+        output = session.run("""
+            public class Main {
+                public static void main(String[] args) {
+                    System.out.println("Hello, World!");
+                }
             }
-        }
-    """)
+        """)
+        print(output)
 
 def execute_javascript_code():
-    run_code('javascript', "console.log('Hello, World!')")
-    run_code('javascript', """
-        const axios = require('axios');
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-            .then(response => console.log(response.data));
-    """, libraries=['axios'])
+    with SandboxSession(lang='javascript', keep_template=True, verbose=True) as session:
+        output = session.run("console.log('Hello, World!')")
+        print(output)
+
+        output = session.run("""
+            const axios = require('axios');
+            axios.get('https://jsonplaceholder.typicode.com/posts/1')
+                .then(response => console.log(response.data));
+        """, libraries=['axios'])
+        print(output)
 
 def execute_cpp_code():
-    run_code('cpp', """
-        #include <iostream>
-        int main() {
-            std::cout << "Hello, World!" << std::endl;
-            return 0;
-        }
-    """)
-    run_code('cpp', """
-        #include <iostream>
-        #include <vector>
-        int main() {
-            std::vector<int> v = {1, 2, 3, 4, 5};
-            for (int i : v) {
-                std::cout << i << " ";
+    with SandboxSession(lang='cpp', keep_template=True, verbose=True) as session:
+        output = session.run("""
+            #include <iostream>
+            int main() {
+                std::cout << "Hello, World!" << std::endl;
+                return 0;
             }
-            std::cout << std::endl;
-            return 0;
-        }
-    """)
-    run_code('cpp', """
-        #include <iostream>
-        #include <vector>
-        #include <algorithm>
-        int main() {
-            std::vector<int> v = {1, 2, 3, 4, 5};
-            std::reverse(v.begin(), v.end());
-            for (int i : v) {
-                std::cout << i << " ";
+        """)
+        print(output)
+
+        output = session.run("""
+            #include <iostream>
+            #include <vector>
+            int main() {
+                std::vector<int> v = {1, 2, 3, 4, 5};
+                for (int i : v) {
+                    std::cout << i << " ";
+                }
+                std::cout << std::endl;
+                return 0;
             }
-            std::cout << std::endl;
-            return 0;
-        }
-    """, libraries=['libstdc++'])
+        """)
+        print(output)
+
+        output = session.run("""
+            #include <iostream>
+            #include <vector>
+            #include <algorithm>
+            int main() {
+                std::vector<int> v = {1, 2, 3, 4, 5};
+                std::reverse(v.begin(), v.end());
+                for (int i : v) {
+                    std::cout << i << " ";
+                }
+                std::cout << std::endl;
+                return 0;
+            }
+        """, libraries=['libstdc++'])
+        print(output)
 
 if __name__ == '__main__':
     execute_python_code()

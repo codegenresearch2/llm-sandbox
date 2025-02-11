fb19eda6c3c996ef0b9e3539c2e2735f549e61fa
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 14:21:10 +0000

    Reset to c949561d913952e7b86528c45de109b119495ce4 and made changes for LLM Round 1

--- llm_sandbox/session.py
+++ llm_sandbox/session.py
@@ -11,14 +11,9 @@ from llm_sandbox.utils import (
     get_libraries_installation_command,
     get_code_file_extension,
     get_code_execution_command,
+    get_code_compilation_command,
 )
-from llm_sandbox.const import (
-    SupportedLanguage,
-    SupportedLanguageValues,
-    DefaultImage,
-    NotSupportedLibraryInstallation,
-)
-
+from llm_sandbox.const import SupportedLanguage, SupportedLanguageValues, DefaultImage, NotSupportedLibraryInstallation
 
 class SandboxSession:
     def __init__(
@@ -29,14 +24,6 @@ class SandboxSession:
         keep_template: bool = False,
         verbose: bool = True,
     ):
-        """
-        Create a new sandbox session
-        :param image: Docker image to use
-        :param dockerfile: Path to the Dockerfile, if image is not provided
-        :param lang: Language of the code
-        :param keep_template: if True, the image and container will not be removed after the session ends
-        :param verbose: if True, print messages
-        """
         if image and dockerfile:
             raise ValueError("Only one of image or dockerfile should be provided")
 
@@ -102,7 +89,6 @@ class SandboxSession:
             self.container = None
 
         if self.is_create_template and not self.keep_template:
-            # check if the image is used by any other container
             containers = self.client.containers.list(all=True)
             image_id = (
                 self.image.id
@@ -132,14 +118,15 @@ class SandboxSession:
                 "Session is not open. Please call open() method before running code."
             )
 
+        commands = []
+
         if libraries:
             if self.lang.upper() in NotSupportedLibraryInstallation:
                 raise ValueError(
                     f"Library installation has not been supported for {self.lang} yet!"
                 )
 
-            command = get_libraries_installation_command(self.lang, libraries)
-            self.execute_command(command)
+            commands.append(get_libraries_installation_command(self.lang, libraries))
 
         code_file = f"/tmp/code.{get_code_file_extension(self.lang)}"
         with open(code_file, "w") as f:
@@ -147,12 +134,12 @@ class SandboxSession:
 
         self.copy_to_runtime(code_file, code_file)
 
-        output = ""
-        commands = get_code_execution_command(self.lang, code_file)
-        for command in commands:
-            output = self.execute_command(command)
+        if self.lang == SupportedLanguage.CPP:
+            commands.append(get_code_compilation_command(self.lang, code_file))
 
-        return output
+        commands.append(get_code_execution_command(self.lang, code_file))
+
+        return commands
 
     def copy_from_runtime(self, src: str, dest: str):
         if not self.container:
@@ -179,7 +166,7 @@ class SandboxSession:
 
         is_created_dir = False
         directory = os.path.dirname(dest)
-        if directory and not self.container.exec_run(f"test -d {directory}")[0] == 0:
+        if directory:
             self.container.exec_run(f"mkdir -p {directory}")
             is_created_dir = True
 
